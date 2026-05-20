d738cea391ec19f13686e40ebccf66fba77e63c6
fso4-glitch <fso4@aluno.ifnmg.edu.br>
2026-05-20 16:13:30 +0000

    fix: caminhos corrigidos css e js

--- css/script.js
+++ css/script.js
@@ -1,6 +1,5 @@
 console.log("Site carregado");
 
-// exemplo de interação
 document.querySelectorAll('.btn').forEach(btn => {
     btn.addEventListener('click', () => {
         console.log('Botão clicado');
