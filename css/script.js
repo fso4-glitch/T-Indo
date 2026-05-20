28a0faa8042d4a0e10f67a10e1f78386c9b250e8
fso4-glitch <fso4@aluno.ifnmg.edu.br>
2026-05-20 16:55:41 +0000

    fix: separando html css js corretamente

--- css/script.js
+++ css/script.js
@@ -1,11 +1,1 @@
-console.log("Tô Indo carregado 🚗");
-
-document.addEventListener("DOMContentLoaded", () => {
-    const botoes = document.querySelectorAll(".btn");
-
-    botoes.forEach(btn => {
-        btn.addEventListener("click", () => {
-            console.log("Redirecionando para Play Store...");
-        });
-    });
-});
+console.log("Tô Indo PRO carregado 🚀");
