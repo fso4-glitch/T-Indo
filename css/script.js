0503466a093abe81f323b67129f0319d2e0cba85
fso4-glitch <fso4@aluno.ifnmg.edu.br>
2026-05-20 16:31:48 +0000

    feat: landing page To Indo completa

--- css/script.js
+++ css/script.js
@@ -1,7 +1,1 @@
-console.log("Tô Indo carregado");
-
-document.querySelectorAll('.btn').forEach(btn => {
-    btn.addEventListener('click', () => {
-        console.log('Clique no botão');
-    });
-});
+console.log("Tô Indo PRO carregado 🚀");
