63dbe72b35968631447495347ff2af1d67f866f4
fso4-glitch <fso4@aluno.ifnmg.edu.br>
2026-05-20 16:10:01 +0000

    feat: site completo funcionando

--- css/script.js
+++ css/script.js
@@ -1,1 +1,8 @@
 console.log("Site carregado");
+
+// exemplo de interação
+document.querySelectorAll('.btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log('Botão clicado');
+    });
+});
